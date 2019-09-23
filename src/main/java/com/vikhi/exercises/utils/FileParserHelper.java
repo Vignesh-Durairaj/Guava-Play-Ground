62d27c61718cc98885c366e7ac06c2cdca57e607
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-09-23 15:59:42 +0800

    Improving coverage

--- src/main/java/com/vikhi/exercises/utils/FileParserHelper.java
+++ src/main/java/com/vikhi/exercises/utils/FileParserHelper.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class FileParserHelper {
 
+	private FileParserHelper() {
+		// No constructor needed for the static class
+	}
+	
 	public static List<String> getAllWords (final File inputFile) {
 		return List.of();
 	}
