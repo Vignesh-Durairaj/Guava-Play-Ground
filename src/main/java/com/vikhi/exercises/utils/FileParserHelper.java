5dae400043ce33508a4c593872c7162f319b8bf4
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-09-24 11:58:38 +0800

    Added logic to parse file to the list of words in it

--- src/main/java/com/vikhi/exercises/utils/FileParserHelper.java
+++ src/main/java/com/vikhi/exercises/utils/FileParserHelper.java
@@ -1,7 +1,11 @@
 package com.vikhi.exercises.utils;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileParserHelper {
 
@@ -9,7 +13,18 @@ public class FileParserHelper {
 		// No constructor needed for the static class
 	}
 	
-	public static List<String> getAllWords (final File inputFile) {
-		return List.of();
+	public static List<String> getAllWords (final Path inputFilePath) {
+		List<String> words = null;
+		try {
+			words = Files
+					.lines(inputFilePath)
+					.map(line -> line.replaceAll("[,.]", "").split(" "))
+					.flatMap(arr -> Arrays.stream(arr))
+					.filter(word -> !"".equals(word))
+					.collect(Collectors.toList());
+		} catch (IOException e) {
+			System.out.println("Encountered IO Exception.");;
+		}
+		return words;
 	}
 }
--- src/test/java/com/vikhi/test/utils/FileParserTest.java
+++ src/test/java/com/vikhi/test/utils/FileParserTest.java
@@ -3,6 +3,7 @@ package com.vikhi.test.utils;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.junit.Test;
@@ -13,8 +14,9 @@ public class FileParserTest {
 
 	@Test
 	public void testAllWordsInFile() {
-		List<String> words = FileParserHelper.getAllWords(null);
+		List<String> words = FileParserHelper.getAllWords(Paths.get("twister.txt"));
 		assertNotNull(words);
-		assertEquals(0, words.size());
+		assertEquals(75, words.size());
+		System.out.println(words);
 	}
 }
