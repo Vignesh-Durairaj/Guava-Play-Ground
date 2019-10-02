8896923fe72a5d76b761cf6d3c5d0b58682288db
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-02 16:12:08 +0530

    Added negative test cases

--- src/test/java/com/vikhi/test/utils/MultiMapTest.java
+++ src/test/java/com/vikhi/test/utils/MultiMapTest.java
@@ -1,5 +1,32 @@
 package com.vikhi.test.utils;
 
-public class MultiMapTest {
+import static org.junit.Assert.fail;
 
+import java.io.File;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vikhi.exercises.collections.MultiMapExercise;
+
+public class MultiMapTest extends BaseTest{
+
+	private MultiMapExercise multiMapFactory;
+	
+	@Before
+	public void init() {
+		multiMapFactory = new MultiMapExercise();
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullFile() {
+		multiMapFactory.getListOfWordsByCount(null);
+		fail("The above line should throw an exception but was not !");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidFile() {
+		multiMapFactory.getListOfWordsByCount("test.test");
+		fail("The above line should throw an exception but was not !");
+	}
 }
