88c86433d30e3ed8ce0957d645b2c28626c02965
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-07 16:46:27 +0530

    Updated DAO method

--- src/main/java/com/vikhi/exercises/dao/EmployeeDao.java
+++ src/main/java/com/vikhi/exercises/dao/EmployeeDao.java
@@ -2,6 +2,8 @@ package com.vikhi.exercises.dao;
 
 import java.util.List;
 
+import com.vikhi.exercises.model.Employee;
+
 public interface EmployeeDao {
 
 	public List<Employee> getAllEmployees();
