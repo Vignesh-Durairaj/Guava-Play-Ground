05ec99ddd4e27db23a40da7cc91f90ff61aa7e23
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-09 12:23:18 +0800

    Added a new test case for pre-conditions

---
+++ src/test/java/com/vikhi/test/utils/PreConditionsTest.java
@@ -0,0 +1,13 @@
+package com.vikhi.test.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PreConditionsTest extends BaseTest {
+
+	@Before
+	public void init() {
+		initData();
+	}
+	
+}
