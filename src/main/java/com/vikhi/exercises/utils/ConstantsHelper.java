364ef275d1d8565436b7470b7eb4fbd7135d25ca
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-14 19:34:11 +0530

    Added more test cases

--- src/main/java/com/vikhi/exercises/utils/ConstantsHelper.java
+++ src/main/java/com/vikhi/exercises/utils/ConstantsHelper.java
@@ -1,14 +1,13 @@
 package com.vikhi.exercises.utils;
 
 public class ConstantsHelper {
-
-	private ConstantsHelper() {
-		// To avoid instantiating outside.
-	}
-	
 	public static final String PIPE_SEPARATOR = "|";
 	public static final String COMMA = ",";
 	public static final String COLON = ":";
 	public static final String SEMI_COLON = ";";
 	public static final String WHITE_SPACE = " ";
+	
+	private ConstantsHelper() {
+		// To avoid instantiating outside.
+	}
 }
--- src/test/java/com/vikhi/test/utils/GuavaStringUtilsTest.java
+++ src/test/java/com/vikhi/test/utils/GuavaStringUtilsTest.java
@@ -5,8 +5,11 @@ import static com.vikhi.exercises.utils.ConstantsHelper.COMMA;
 import static com.vikhi.exercises.utils.ConstantsHelper.PIPE_SEPARATOR;
 import static com.vikhi.exercises.utils.ConstantsHelper.SEMI_COLON;
 import static com.vikhi.exercises.utils.ConstantsHelper.WHITE_SPACE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
 import java.util.List;
@@ -14,12 +17,16 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.vikhi.exercises.utils.ConstantsHelper;
 
 public class GuavaStringUtilsTest extends BaseTest{
 	
 	private List<Integer> ints;
 	private Joiner joiner;
+	private Splitter splitter;
 	
 	@BeforeEach
 	void init() {
@@ -54,13 +61,44 @@ public class GuavaStringUtilsTest extends BaseTest{
 	@Test
 	void testComplexObjectJoiner() {
 		List<Object> objects = List.of(mobileNumDao.getAllMobileNumbers().get(0), personDao.getPersons().get(0));
-		Joiner joiner = Joiner.on(COLON);
+		Joiner joiner = Joiner.on(ConstantsHelper.COLON);
 		String newString = mobileNumDao.getAllMobileNumbers().get(0).toString() + COLON + personDao.getPersons().get(0).toString();
 		assertEquals(newString, joiner.join(objects));
 	}
 	
 	@Test
 	void testSplitter() {
+		splitter = Splitter.on(COLON);
+		assertThat(splitter.split("1::2:3:4::5:"), containsInAnyOrder("1","","2","3","4","","5",""));
 		
+		splitter = splitter.omitEmptyStrings();
+		assertThat(splitter.split("1:: 2:3:4 ::5:"), containsInAnyOrder("1"," 2","3","4 ","5"));
+		
+		splitter = splitter.trimResults();
+		assertThat(splitter.split("1:: 2:3:4 ::5:"), containsInAnyOrder("1","2","3","4","5"));
+		
+		splitter = Splitter.on(CharMatcher.anyOf(",:;_- ")).omitEmptyStrings();
+		assertThat(splitter.split("a,b;c_d-e f g"), containsInAnyOrder("a", "b", "c", "d", "e", "f", "g"));
+		
+		splitter = Splitter.on(COMMA + WHITE_SPACE);
+		assertThat(splitter.split("Vignesh, Durairaj, Tirupur"), containsInAnyOrder("Vignesh", "Durairaj", "Tirupur"));
+		
+		splitter = Splitter.fixedLength(3);
+		assertThat(splitter.split("Vignesh"), containsInAnyOrder("Vig", "nes", "h"));
+		
+		splitter = Splitter
+				.on(COMMA)
+				.omitEmptyStrings()
+				.trimResults()
+				.limit(4);
+		assertThat(splitter.split("1, 2, 3, 4, 5, 6, 7, 8"), containsInAnyOrder("1", "2", "3", "4, 5, 6, 7, 8"));
+		
+		assertThat(Splitter
+				.on(COMMA)
+				.trimResults(CharMatcher.is('_'))
+				.split("_a ,_b_ ,c__"), 
+				containsInAnyOrder("a ", "b_ ", "c"));
+		
+		assertTrue((splitter.splitToList("a, b, c , d,e")) instanceof List);
 	}
 }
