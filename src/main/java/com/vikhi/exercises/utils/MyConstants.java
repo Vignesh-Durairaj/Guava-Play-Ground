1b159591af26173ad4834ba4de8f360a739d2fe9
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-10-14 18:00:24 +0530

    Code coverage improvement

--- src/main/java/com/vikhi/exercises/utils/MyConstants.java
+++ src/main/java/com/vikhi/exercises/utils/MyConstants.java
@@ -2,6 +2,10 @@ package com.vikhi.exercises.utils;
 
 public class MyConstants {
 
+	private MyConstants() {
+		throw new UnsupportedOperationException("Object instantiation not allowed for constant classes");
+	}
+	
 	public static final String PIPE_SEPARATOR = "|";
 	public static final String COMMA = ",";
 	public static final String COLON = ":";
